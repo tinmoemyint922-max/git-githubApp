548a95ef2687ca2752eb3b56cca5d257e7ede7db
Tin Moe Myint <tinmoemyint922@gmail.com>
2026-05-26 13:56:38 -0700

    fix port for deploy

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port" + PORT);
 });
